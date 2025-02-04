d2f0c1e89ece3c75c29faa6f6dfe2fc88bf0cf9e
Stephen Gunawan <stephen_gunawan2014@yahoo.com.au>
2025-02-04 22:25:45 +1100

    finalised Etch-a-Sketch project
    finalised javascript logic related to cell sizing, changed css, and completed readme

--- js-script.js
+++ js-script.js
@@ -7,17 +7,20 @@ button.addEventListener('click', function(){
     let size = prompt('Enter Size of Grid');
         if (size>0 && size <=100){
         container.innerHTML= '';
-        container.style.setProperty('--sizeCont', `${size *20}px`)
         gridMaker(size, size);
     }
 });
 
-function gridMaker(gridHeight, gridWidth){
-    for (let i = 0; i<gridHeight*gridWidth; i++){
+function gridMaker(size){
+    for (let i = 0; i<size*size; i++){
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.height = `${500/size}px`;
+        square.style.width = `${500/size}px`;
+        square.style.border = '0.5px solid black';
+        square.style.boxSizing = 'border-box';
         square.addEventListener('mouseover',function(){
-            square.style.backgroundColor = 'blue';
+            square.style.backgroundColor = 'black';
         })
         container.appendChild(square);
     }
